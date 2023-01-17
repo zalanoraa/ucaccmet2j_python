e867b7d9777406ff6bc160cae151635879ea5ffc
Nora Zala <zalanoraa@gmail.com>
2023-01-17 16:12:05 +0100

    part 2 done

--- results.py
+++ results.py
@@ -1,3 +1,4 @@
+#part 1
 import json
 with open('precipitation.json') as file:
     precipitation = json.load(file)
@@ -25,11 +26,35 @@ print(total_monthly_precipitation)
 
 
 
+#with open ('results.json', 'w') as f:
+#    json.dump ({
+#        "Seattle": {
+#            "station":"GHCND:US1WAKG0038",
+#            "state": "WA",
+#            "total_monthly_precipitation": total_monthly_precipitation
+#            }
+#        }, f, indent=4, sort_keys=True)
+
+#part 2
+months_sum = 0
+for month in total_monthly_precipitation:
+    months_sum += month
+total_yearly_precipitation = months_sum
+print(total_yearly_precipitation)
+
+relative_monthly_precipitation = []
+for month in total_monthly_precipitation:
+    relative_monthly_precipitation.append(month/total_yearly_precipitation)
+print(relative_monthly_precipitation)
+
+
 with open ('results.json', 'w') as f:
     json.dump ({
         "Seattle": {
             "station":"GHCND:US1WAKG0038",
             "state": "WA",
-            "total_monthly_precipitation": total_monthly_precipitation
+            "total_monthly_precipitation": total_monthly_precipitation,
+            "total_yearly_precipitation": total_yearly_precipitation,
+            "relative_monthly_precipitation": relative_monthly_precipitation,
             }
         }, f, indent=4, sort_keys=True)
