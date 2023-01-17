1f9eb40456890d31b19eafc621b0fa49b29c7d3d
Nora Zala <zalanoraa@gmail.com>
2023-01-17 15:47:59 +0100

    part 1: monthly precipitation

---
+++ results.py
@@ -0,0 +1,35 @@
+import json
+with open('precipitation.json') as file:
+    precipitation = json.load(file)
+#print(precipitation)
+
+#from csv import DictReader
+#with open('stations.csv') as file:
+#    reader = DictReader(file)
+#    stations = list(reader) 
+#print(stations)
+
+seattle_data = []
+for data in precipitation:
+    if data['station'] == 'GHCND:US1WAKG0038':
+        seattle_data.append(data)
+print(seattle_data)
+
+total_monthly_precipitation=[0] * 12
+for data in seattle_data:
+    date = data['date']
+    date = date.split("-")
+    value = data['value']
+    total_monthly_precipitation[int(date[1])-1] += value
+print(total_monthly_precipitation)
+
+
+
+with open ('results.json', 'w') as f:
+    json.dump ({
+        "Seattle": {
+            "station":"GHCND:US1WAKG0038",
+            "state": "WA",
+            "total_monthly_precipitation": total_monthly_precipitation
+            }
+        }, f, indent=4, sort_keys=True)
